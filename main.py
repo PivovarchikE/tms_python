98d7403306f94959416250532e710d2d6b17244e
Evgeniy Pivovarchik <crshfacer.prol@gmail.com>
2025-10-22 20:20:40 +0300

    add console app with raw_provider

---
+++ main.py
@@ -0,0 +1,10 @@
+from console_ui import  ConsoleDbApp
+from providers.raw_provider import RawSqlProvider
+
+
+def main():
+    app = ConsoleDbApp(RawSqlProvider,RawSqlProvider)
+    app.start_app()
+
+if __name__=='__main__':
+    main()
